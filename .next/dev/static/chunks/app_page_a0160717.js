7c6c2e404996f90b246a666a660603f82449bc50
Mohamed anas <mdanasbinsaleem@gmail.com>
2026-01-12 10:23:12 +0530

    Ignore Next.js build files

--- .next/dev/static/chunks/app_page_a0160717.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_bdf14468._.css",
-  "static/chunks/node_modules_next_dist_279f65b1._.js"
-],
-    source: "dynamic"
-});
